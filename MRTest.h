cd63050e21bcae0bbc9cb7c3c536e5e71cc54794
Marjan Radanovic <mradanov@Marjans-MacBook-Pro.local>
2014-04-01 00:46:49 -0700

    added test class

---
+++ MRTest.h
@@ -0,0 +1,13 @@
+//
+//  MRTest.h
+//  Test GIt
+//
+//  Created by Marjan Radanovic on 4/1/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MRTest : NSObject
+
+@end
---
+++ MRTest.m
@@ -0,0 +1,13 @@
+//
+//  MRTest.m
+//  Test GIt
+//
+//  Created by Marjan Radanovic on 4/1/14.
+//
+//
+
+#import "MRTest.h"
+
+@implementation MRTest
+
+@end
